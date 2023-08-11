e2a6feea5ac2da0ba290f59ee03eeff0ac138509
Bruna Coutinho <smarciabruna@gmail.com>
2023-08-10 23:16:06 -0300

    edição 6

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import { Text, View, Image, TouchableOpacity, ToastAndroid } from 'react-native';
 import styles from '../Styles/HomeScreenStyle';
 import { useNavigation } from '@react-navigation/native';
 import auth, { firebase } from "@react-native-firebase/auth"
 import FontAwesome from 'react-native-vector-icons/dist/FontAwesome';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 const HomeScreen = () => {
   const navigation = useNavigation();
   const [photo, setPhoto] = useState('')
@@ -18,20 +19,32 @@ const HomeScreen = () => {
       photoUser = user.photoURL
     console.log(user, userInfo)
   });
-   
+
+  const imagePicker = () => {
+    let options = {
+      storageOptions: {
+        path: 'image'
+      }
+    }
+    launchImageLibrary(options, response => {
+      setPhoto(response.assets[0].uri)
+      console.log(response.assets[0].uri)
+    })
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.userCard}>
         <View>
           <TouchableOpacity>
-            <Image source={{ uri: "https://avatars.githubusercontent.com/u/97321917?v=4" }} style={styles.userPhoto} />
+            <Image source={{ uri: photo }} style={styles.userPhoto} />
           </TouchableOpacity>
         </View>
         <View style={styles.userInfo}>
           <Text style={styles.userName}>{fullname}</Text>
           <Text style={styles.userFollowers}>{emailuser}</Text>
         </View>
-        <TouchableOpacity style={styles.editButton}>
+        <TouchableOpacity onPress={() => { imagePicker() }} style={styles.editButton}>
           <Text style={styles.editButtonText}>Edit</Text>
         </TouchableOpacity>
       </View>
